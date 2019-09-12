a7d98c30a11a8edfdf21f539637f009148058f5f
fiiniefi <marcin.drewniak1@vp.pl>
2019-09-12 15:18:44 +0200

    Code refactor
    Variable names improvement, lists to generators, break statement removed, etc.

--- weirdtext/weirdtext.py
+++ weirdtext/weirdtext.py
@@ -15,34 +15,37 @@ def encode(text):
     return "".join(encoded_words), sorted(re.split(r'\W+', text), key=str.lower)
 
 
-def decode(text, array):
+def decode(text, original_words):
     """
     Decodes text encoded by Weirdtext algorithm
 
     :param str text: Text to decode
-    :param list array: list of decoded words
+    :param list original_words: list of decoded words
     :rtype: str
     :return: decoded text
     """
     words = get_words_with_delimiters(text)
-    decoded_words = get_coded_words(words, decode_word, array)
+    decoded_words = get_coded_words(words, decode_word, original_words)
     return "".join(decoded_words)
 
 
 def encode_word(word):
-    if len(word) < 4 or has_one_unique_letter(word[1:-1]):
+    if len(word) < 4 or consists_of_one_unique_letter(word[1:-1]):
         return word
+    return next(encoded for encoded in randomize_word(word) if encoded is not None)
+
+
+def randomize_word(word):
+    #  non-deterministic
+    letters = list(word[1:-1])
     while True:
-        letters = list(word[1:-1])
         random.shuffle(letters)
         encoded = "".join(letters)
-
-        if encoded != word[1:-1]:
-            return word.replace(word[1:-1], encoded)
+        yield word.replace(word[1:-1], encoded) if encoded != word[1:-1] else None
 
 
-def decode_word(word, array):
-    for pattern in array:
+def decode_word(word, original_words):
+    for pattern in original_words:
         if sorted(word) == sorted(pattern):
             return pattern
 
@@ -51,10 +54,10 @@ def get_words_with_delimiters(text):
     return re.split(r'(\W+)', text)
 
 
-def get_coded_words(words, coder, *args):
-    return [coder(word, *args) if word.isalpha() else word
-            for word in words]
+def get_coded_words(words, codec, *args):
+    for word in words:
+        yield codec(word, *args) if word.isalpha() else word
 
 
-def has_one_unique_letter(word):
-    return len(list(dict.fromkeys(word))) == 1
+def consists_of_one_unique_letter(word):
+    return len(set(word)) == 1
