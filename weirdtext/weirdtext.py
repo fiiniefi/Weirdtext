fad191d7ecfd2684eefecbb1fd6f4effba2acf12
fiiniefi <marcin.drewniak1@vp.pl>
2019-09-12 03:04:12 +0200

    Add docstrings to client's methods

--- weirdtext/weirdtext.py
+++ weirdtext/weirdtext.py
@@ -3,12 +3,27 @@ import random
 
 
 def encode(text):
+    """
+    Encodes text using Weirdtext algorithm
+
+    :param str text: Text to encode
+    :rtype: Tuple[str, list]
+    :return: pair (encoded_text, sorted_input_words)
+    """
     words = get_words_with_delimiters(text)
     encoded_words = get_coded_words(words, encode_word)
     return "".join(encoded_words), sorted(re.split('\\W+', text), key=str.lower)
 
 
 def decode(text, array):
+    """
+    Decodes text encoded by Weirdtext algorithm
+
+    :param str text: Text to decode
+    :param list array: list of decoded words
+    :rtype: str
+    :return: decoded text
+    """
     words = get_words_with_delimiters(text)
     decoded_words = get_coded_words(words, decode_word, array)
     return "".join(decoded_words)
